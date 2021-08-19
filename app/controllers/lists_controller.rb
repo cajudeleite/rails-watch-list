92e1fd474e43d214df350a66c3a33d5b324e5a95
Nathan Debaz <nathandebaz266@gmail.com>
2021-08-19 16:41:01 -0300

    primeiro dia feito

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+
+before_action :set_list, only: [:show]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  def movies
+    @movies = List.find(params[:id]).movies
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
