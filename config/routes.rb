aa44a55a9ea82e001c9a674f041855c8f946bb0e
Nathan Debaz <nathandebaz266@gmail.com>
2021-08-20 17:04:35 -0300

    ja ta chato

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only:[:destroy]
+  resources :bookmarks, only: [:destroy]
   resources :reviews, only: :destroy
 end
